bd8cbcc1615117860af039bed52ccb0fb695e393
Ksenia Troshchenkova <ksenia9660134286@Ksenias-Air.home>
2023-03-24 11:54:08 +0100

    Add new game: calculator

---
+++ brain_games/calc_module.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import prompt
+import random
+from .game_module import play_game
+
+
+def evaluate_calculation(number1, number2, operation):
+    if operation == "+":
+        return (number1 + number2)
+    elif operation == "-":
+        return (number1 - number2)
+    if operation == "*":
+        return (number1 * number2)
+
+
+def play_round():
+    number1 = random.randint(1, 100)
+    number2 = random.randint(1, 100)
+    operation = random.choice(["+", "-", "*"])
+    calculation_result = evaluate_calculation(number1, number2, operation)
+    print(f"Question: {number1} {operation} {number2}")
+    answer = prompt.string("Your answer: ")
+    answer = answer.strip()
+    if not answer.lstrip("-").isnumeric():
+        string = "You entered invalid answer."
+    else:
+        if int(answer) == calculation_result:
+            string = ""
+        else:
+            string = f"{answer} is wrong answer ;(. "\
+                f"Correct answer was {calculation_result}."
+    return string
+
+
+def play_calculator():
+    play_game('What is the result of the expression?', play_round)
---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from ..calc_module import play_calculator
+
+
+def main():
+    play_calculator()
+
+
+if __name__ == "__main__":
+    main()
